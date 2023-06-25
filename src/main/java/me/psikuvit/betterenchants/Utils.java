d9556008eb6ab846894d797d421cf4b41ee26946
Psikuvit <username@whatever.com>
2023-06-25 20:14:45 +0200

    moved enchant stuff to EnchantUtils

--- src/main/java/me/psikuvit/betterenchants/Utils.java
+++ src/main/java/me/psikuvit/betterenchants/Utils.java
@@ -1,26 +1,6 @@
 package me.psikuvit.betterenchants;
 
-import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.enchantments.EnchantmentWrapper;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.Field;
-import java.util.Objects;
-
 public class Utils {
-
-    public static final NamespacedKey GLOW_ENCHANTMENT_KEY = new NamespacedKey(BetterEnchants.getPlugin(), "glow");
-    private static final Enchantment instance;
-
-    static {
-        Enchantment existing = Enchantment.getByKey(GLOW_ENCHANTMENT_KEY);
-        instance = Objects.requireNonNullElseGet(existing, GlowEnch::new);
-        registerEnchantment(instance);
-    }
-
 
     public static String convertToRomanNumeral(int number) {
         if (number < 1 || number > 100) {
@@ -40,25 +20,4 @@ public class Utils {
 
         return romanNumeral.toString();
     }
-    public static boolean addGlowEffect(@NotNull ItemMeta meta) {
-        if (meta.hasEnchant(instance)) {
-            return false;
-        }
-        meta.addEnchant(instance, 1, true);
-        return true;
-    }
-
-    public static void registerEnchantment(final Enchantment enchantment) {
-        try {
-            final Field fieldAcceptingNew = Enchantment.class.getDeclaredField("acceptingNew");
-            fieldAcceptingNew.setAccessible(true);
-            fieldAcceptingNew.set(null, true);
-            fieldAcceptingNew.setAccessible(false);
-
-            Enchantment.registerEnchantment(enchantment);
-        } catch (final Throwable exception) {
-            exception.printStackTrace();
-        }
-    }
-
 }
